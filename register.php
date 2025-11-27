6cf28c8d0763f0133c89953d00559546233d1ae9
Sensei-V23 <zaytmop@gmail.com>
2025-11-27 19:17:22 +0700

    Add files via upload

--- register.php
+++ register.php
@@ -1,11 +1,22 @@
 <?php
     $conn = new mysqli("localhost", "root", "", "mydatainfo");
 
+    if ($conn->connect_error) {
+        die("เชื่อมต่อฐานข้อมูลไม่ได้: " . $conn->connect_error);
+    }
+
     $username = $_POST['username'];
     $password = $_POST['password'];
 
+    // ป้องกันค่าว่าง
+    if (empty($username) || empty($password)) {
+        die("กรุณากรอกข้อมูลให้ครบ!");
+    }
+
+    // SQL
     $sql = "INSERT INTO users (Username, Password) VALUES ('$username', '$password')";
-    $conn->($sql);
+
+    if ($conn->query($sql));
 
     $conn->close();
 ?>
