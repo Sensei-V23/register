ba8d128387d7a5feb99bdf02795d3a8bfa651d59
Sensei-V23 <zaytmop@gmail.com>
2025-11-27 18:55:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function onsubmitregister() {
+    const name = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    alert("Username: " + name + "\nPassword: " +password)
+}
